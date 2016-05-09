c2bf03993949c618a20e3262a5761b1400da29c5
hellenRojas <kari0695@gmailcom>
2016-05-08 21:21:58 -0600

    Cambios

--- Carrera/app/src/main/java/cr/ac/itcr/carrera/activity/AgregarEventos.java
+++ Carrera/app/src/main/java/cr/ac/itcr/carrera/activity/AgregarEventos.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import cr.ac.itcr.carrera.R;
 
@@ -65,7 +68,20 @@ public class AgregarEventos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar_eventos, container, false);
+        final View view = inflater.inflate(R.layout.fragment_agregar_eventos, container, false);
+        final Spinner etTipo= (Spinner)view.findViewById(R.id.spTEveAgreg);
+        final EditText etNombre= (EditText)view.findViewById(R.id.etNombreEveAgregar);
+        final EditText etDetalles= (EditText)view.findViewById(R.id.etDetallesEveAgreg);
+
+        final Button btnAgregar = (Button)view.findViewById(R.id.btnEvenAgreg);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
