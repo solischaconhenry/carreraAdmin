db64077c550c2c3aa4bec7b0fcda77f30872d57b
hsolicha <hsolicha@hotmail.com>
2016-05-08 21:31:21 -0600

    eventos

--- Carrera/app/src/main/java/cr/ac/itcr/carrera/activity/ChatRoomActivity.java
+++ Carrera/app/src/main/java/cr/ac/itcr/carrera/activity/ChatRoomActivity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -64,7 +65,6 @@ public class ChatRoomActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         inputMessage = (EditText) findViewById(R.id.message);
-        btnSend = (Button) findViewById(R.id.btn_send);
 
         Intent intent = getIntent();
         chatRoomId = intent.getStringExtra("chat_room_id");
@@ -102,12 +102,6 @@ public class ChatRoomActivity extends AppCompatActivity {
             }
         };
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage();
-            }
-        });
 
         fetchChatThread();
     }
@@ -146,108 +140,6 @@ public class ChatRoomActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Posting a new message in chat room
-     * will make an http call to our server. Our server again sends the message
-     * to all the devices as push notification
-     * */
-    private void sendMessage() {
-        final String message = this.inputMessage.getText().toString().trim();
-
-        if (TextUtils.isEmpty(message)) {
-            Toast.makeText(getApplicationContext(), "Enter a message", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        String endPoint = EndPoints.CHAT_ROOM_MESSAGE.replace("_ID_", chatRoomId);
-
-        Log.e(TAG, "endpoint: " + endPoint);
-
-        this.inputMessage.setText("");
-
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                endPoint, new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                Log.e(TAG, "response: " + response);
-
-                try {
-                    JSONObject obj = new JSONObject(response);
-
-                    // check for error
-                    if (obj.getBoolean("error") == false) {
-                        JSONObject commentObj = obj.getJSONObject("message");
-
-                        String commentId = commentObj.getString("message_id");
-                        String commentText = commentObj.getString("message");
-                        String createdAt = commentObj.getString("created_at");
-
-                        JSONObject userObj = obj.getJSONObject("user");
-                        String userId = userObj.getString("user_id");
-                        String userName = userObj.getString("name");
-                        User user = new User(userId, userName, null);
-
-                        Message message = new Message();
-                        message.setId(commentId);
-                        message.setMessage(commentText);
-                        message.setCreatedAt(createdAt);
-                        message.setUser(user);
-
-                        messageArrayList.add(message);
-
-                        mAdapter.notifyDataSetChanged();
-                        if (mAdapter.getItemCount() > 1) {
-                            // scrolling to bottom of the recycler view
-                            recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, mAdapter.getItemCount() - 1);
-                        }
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "" + obj.getString("message"), Toast.LENGTH_LONG).show();
-                    }
-
-                } catch (JSONException e) {
-                    Log.e(TAG, "json parsing error: " + e.getMessage());
-                    Toast.makeText(getApplicationContext(), "json parse error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                NetworkResponse networkResponse = error.networkResponse;
-                Log.e(TAG, "Volley error: " + error.getMessage() + ", code: " + networkResponse);
-                Toast.makeText(getApplicationContext(), "Volley error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                inputMessage.setText(message);
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("user_id", MyApplication.getInstance().getPrefManager().getUser().getId());
-                params.put("message", message);
-
-                Log.e(TAG, "Params: " + params.toString());
-
-                return params;
-            };
-        };
-
-
-        // disabling retry policy so that it won't make
-        // multiple http calls
-        int socketTimeout = 0;
-        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-
-        strReq.setRetryPolicy(policy);
-
-        //Adding request to request queue
-        MyApplication.getInstance().addToRequestQueue(strReq);
-    }
-
 
     /**
      * Fetching all the messages of a single chat room
@@ -277,24 +169,14 @@ public class ChatRoomActivity extends AppCompatActivity {
                             String commentId = commentObj.getString("message_id");
                             String commentText = commentObj.getString("message");
                             String createdAt = commentObj.getString("created_at");
+                            String evento = commentObj.getString("evento");
+                            String tipo = commentObj.getString("tipo");
 
-                            JSONObject userObj = commentObj.getJSONObject("user");
-                            String userId = userObj.getString("user_id");
-                            String userName = userObj.getString("username");
-                            User user = new User(userId, userName, null);
+                            TextView txtevento = (TextView)findViewById(R.id.shownameevent);
+                            TextView txtdetalle =(TextView)findViewById(R.id.deatallesevento);
+                            TextView txttipo =(TextView)findViewById(R.id.typeevent);
+                            txtevento.setText(evento);
 
-                            Message message = new Message();
-                            message.setId(commentId);
-                            message.setMessage(commentText);
-                            message.setCreatedAt(createdAt);
-                            message.setUser(user);
-
-                            messageArrayList.add(message);
-                        }
-
-                        mAdapter.notifyDataSetChanged();
-                        if (mAdapter.getItemCount() > 1) {
-                            recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, mAdapter.getItemCount() - 1);
                         }
 
                     } else {
--- Carrera/app/src/main/java/cr/ac/itcr/carrera/app/EndPoints.java
+++ Carrera/app/src/main/java/cr/ac/itcr/carrera/app/EndPoints.java
@@ -8,7 +8,7 @@ public class EndPoints {
     // localhost url
     // public static final String BASE_URL = "http://192.168.0.101/gcm_chat/v1";
 
-    public static final String BASE_URL = "http://192.168.0.8:5050/gcm_chat/v1";
+    public static final String BASE_URL = "http://192.168.0.5:5050/gcm_chat/v1";
     public static final String LOGIN = BASE_URL + "/user/login";
     public static final String USER = BASE_URL + "/user/_ID_";
     public static final String CHAT_ROOMS = BASE_URL + "/chat_rooms";
