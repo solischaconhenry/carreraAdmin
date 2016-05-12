368cc6f4e2673dc93b7df5c734796f77e532fc5f
Antony Durán Hernández <ajdurancr@gmail.com>
2016-05-11 20:32:40 -0600

    actualización de métodos

--- api/controllers/eventosController.js
+++ api/controllers/eventosController.js
@@ -47,35 +47,3 @@ exports.getContenidoEvento = function(eRequest, eResponse) {
         eResponse.send(data);
     });
 };
-
-
-eventosService.nuevoEvento({nombre: 'nombre actividad', descripcion: 'descripcion evento'}, function(data){
-      console.log('nuevoEvento: console.log(data);');
-      console.log(data);
-      eventosService.getEventos(function(data){
-          console.log('getEventos: console.log(data);');
-          console.log(data);
-      });
-      /*eventosService.editarEvento(eRequest.params.idEvento, eRequest.body, function(data){
-          eResponse.send(data);
-      });*/
-    });
-
-
-eventosService.getEventoPorId('573383e2cd3c468f2ad3ea23', function(data){
-    console.log('getEventoPorId: console.log(data);');
-    console.log(data);
-});
-
-eventosService.eliminarEvento('573383e2cd3c468f2ad3ea23', function(data){
-    console.log('eliminarEvento: console.log(data);');
-    console.log(data);
-});
-
-/*
-eventosService.editarEvento('573383e2cd3c468f2ad3ea23', {nombre: 'actividad a borrar ------------>'}, function(data){
-      console.log('\n\n-----------------------------\n')
-      console.log('editarEvento: console.log(data);');
-      console.log(data);
-      console.log('\n-----------------------------\n\n')
-    });*/
