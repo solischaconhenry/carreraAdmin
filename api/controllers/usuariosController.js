89c7812530fe3e605f2d4e957e04cc4443ffec1a
Antony Durán Hernández <ajdurancr@gmail.com>
2016-05-11 20:33:02 -0600

    actualización de métodos controlador de usuarios

--- api/controllers/usuariosController.js
+++ api/controllers/usuariosController.js
@@ -6,18 +6,8 @@ exports.bloquearUsuario = function(uRequest, uResponse) {
     });
 };
 
-usuariosService.bloquearUsuario({idUsuario: '573396123eeb7b0cdd843c9a', bloqueado: 0}, function(data){
-        console.log('usuariosService.bloquearUsuario: console.log(data);');
-        console.log(data);
-    });
-
 exports.hacerAdmin= function(uRequest, uResponse) {
   usuariosService.hacerAdmin(uRequest.body, function(data){
         uResponse.send(data);
     });
 };
-
-usuariosService.hacerAdmin({idUsuario: '573396123eeb7b0cdd843c9a', admin: 1}, function(data){
-        console.log('usuariosService.bloquearUsuario: console.log(data);');
-        console.log(data);
-    });
